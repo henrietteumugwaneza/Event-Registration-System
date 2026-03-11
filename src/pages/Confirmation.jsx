cfa19656d636ee0f8c50d436e55f309d9fdc80b9
henrietteumugwaneza <umugwanezahenriette13@gmail.com>
2026-03-11 09:08:13 +0200

    confirmation page

---
+++ src/pages/Confirmation.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+function Confirmation() {
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+
+      <h1 className="text-4xl font-bold mb-4">
+        Registration Successful 🎉
+      </h1>
+
+      <p className="text-lg">
+        Thank you for registering for the Tech Innovation Conference.
+      </p>
+
+      <p>Your seat has been reserved.</p>
+
+    </div>
+  );
+}
+
+export default Confirmation;
